369ba443ca73f1a19bcdad6345b63c4826f2441f
pperez29 <pperez29@externos.bancoestado.cl>
2022-01-13 17:58:27 -0300

    docs agregado

--- src/componentes/gifGridItem.js
+++ src/componentes/gifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const GifgridItem = ({id, title, url}) => {
 
-    console.log(id, title, url);
+    // console.log(id, title, url);
 
     return (
         <div className='card animate__animated animate__fadeIn'>
